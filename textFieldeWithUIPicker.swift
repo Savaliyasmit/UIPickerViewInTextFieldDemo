443e585cd2321a3e57e8b89c8441b5a9e0fc3b8e
Smit Savaliya <137071239+Savaliyasmit@users.noreply.github.com>
2024-12-25 04:39:00 -0800

    Update textFieldeWithUIPicker.swift

--- textFieldeWithUIPicker.swift
+++ textFieldeWithUIPicker.swift
@@ -13,19 +13,39 @@ class textFieldeWithUIPicker:UIViewController {
     
     
     @IBOutlet weak var selectCountryTxt: UITextField!
-     private  var countryPicker:UIPickerView!
+    private  var countryPicker:UIPickerView!
     
     let country:[String] =  ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuad"]
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let toolBar = UIToolbar()
+               toolBar.barStyle = UIBarStyle.default
+               toolBar.isTranslucent = true
+               toolBar.tintColor = UIColor.black
+               toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.dismissPicker))
+       let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        doneButton.tintColor = .systemBlue
+              toolBar.setItems([ spaceButton, doneButton], animated: false)
+              toolBar.isUserInteractionEnabled = true
+
+        
         countryPicker = UIPickerView()
         countryPicker.delegate = self
-        selectCountryTxt.inputView = countryPicker
         selectCountryTxt.clearButtonMode = .whileEditing
+        selectCountryTxt.placeholder = "Select Country"
+        
+        selectCountryTxt.inputView = countryPicker
+        selectCountryTxt.inputAccessoryView = toolBar
+       
+        
     }
-
+    @objc func dismissPicker() {
+            self.view.endEditing(true)
+        }
     
 }
 
